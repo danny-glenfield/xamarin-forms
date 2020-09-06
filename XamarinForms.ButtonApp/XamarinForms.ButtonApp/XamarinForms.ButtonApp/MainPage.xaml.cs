9079518523d8408d0f3a30a77980ee3d9aa104e1
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-06 18:49:03 -0400

    Add ButtonApp solution.

---
+++ XamarinForms.ButtonApp/XamarinForms.ButtonApp/XamarinForms.ButtonApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms.ButtonApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButton_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Click me again!";
+        }
+    }
+}
