666ff466bcfdc224ad377a987fb57eee6325a31b
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-06 19:41:11 -0400

    Add EditorApp solution.

---
+++ XamarinForms.EditorApp/XamarinForms.EditorApp/XamarinForms.EditorApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms.EditorApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEditor_Completed(object sender, EventArgs e)
+        {
+            string text = ((Editor)sender).Text;
+        }
+    }
+}
