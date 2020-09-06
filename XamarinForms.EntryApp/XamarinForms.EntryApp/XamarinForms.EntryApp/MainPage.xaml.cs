375d93be38eafaa4d1bc71cf0fb16e6db9129d9d
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-06 19:14:48 -0400

    Add EntryApp solution.

---
+++ XamarinForms.EntryApp/XamarinForms.EntryApp/XamarinForms.EntryApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms.EntryApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntry_Completed(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+}
