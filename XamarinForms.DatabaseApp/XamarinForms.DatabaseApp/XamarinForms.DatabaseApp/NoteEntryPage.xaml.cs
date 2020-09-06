ac2a76a9a71dd2cdb108d200fb53df2d53c49a11
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-06 18:01:17 -0400

    Add DatabaseApp solution.

---
+++ XamarinForms.DatabaseApp/XamarinForms.DatabaseApp/XamarinForms.DatabaseApp/NoteEntryPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinForms.DatabaseApp.Models;
+
+namespace XamarinForms.DatabaseApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnDeleteButton_Clicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+
+        private async void OnSaveButton_Clicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            note.Date = DateTime.UtcNow;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
