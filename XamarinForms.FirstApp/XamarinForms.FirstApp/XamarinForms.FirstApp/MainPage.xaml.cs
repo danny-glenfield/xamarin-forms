ac206f033d22b09a5b58dd2497a64182d2fd34dd
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-06 17:48:48 -0400

    Add XamarinForms.FirstApp solution.

---
+++ XamarinForms.FirstApp/XamarinForms.FirstApp/XamarinForms.FirstApp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms.FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
