f1fe8b78380c114c3d591f6626c00192951b6b54
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-07 19:54:45 -0400

    Add XamarinForms.WebService solution.

---
+++ Tutorials/XamarinForms.WebService/XamarinForms.WebService/XamarinForms.WebService/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinForms.WebService
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "INSERT_API_KEY_HERE";
+    }
+}
