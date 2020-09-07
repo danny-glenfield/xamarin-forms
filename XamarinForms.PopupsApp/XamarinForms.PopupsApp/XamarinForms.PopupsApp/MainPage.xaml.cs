fc0d74520e085a7eacd8ca557aabba0732789eba
Danny Glenfield <danny.glenfield@gmail.com>
2020-09-07 12:12:51 -0400

    Add PopupsApp solution.

---
+++ XamarinForms.PopupsApp/XamarinForms.PopupsApp/XamarinForms.PopupsApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinForms.PopupsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnDisplayAlertButton_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "This is an alert.", "OK");
+        }
+
+        async void OnDisplayAlertQuestionButton_Clicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Save?", "Would you like to save your data?", "Yes", "No");
+            Console.WriteLine($"Save data: {response}");
+        }
+
+        async void OnDisplayActionSheetButton_Clicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Console.WriteLine($"Action: {action}");
+        }
+    }
+}
